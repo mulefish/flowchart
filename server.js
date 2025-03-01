d7d1f209fb2d60cdf88ad9b3dc4f63052d0da3fb
Paul Montgomery <finch.wren.swift@gmail.com>
2025-03-01 08:10:23 -0800

    Sat 2

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/complete', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'complete.html'));
+});
 app.get('/telos', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'telos.html'));
 });
